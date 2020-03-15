dccaa9bb72caa153b12808feb034c78f1b4e9898
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-03-15 15:50:34 -0500

    initial setup

---
+++ server/routes/chirps.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const chirpsStore = require('../filestore');
+let router = express.Router();
+
+router.get('/:id?', (req, res) => {
+    let id = req.params.id;
+    if (id) {
+        res.json(chirpsStore.GetChirp(id));
+    } else {
+        res.send(chirpsStore.GetChirps());
+    }
+});
+
+router.post('/', (req, res) => {
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200);
+});
+
+
+
+module.exports = router;
+
